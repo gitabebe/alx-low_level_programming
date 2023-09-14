0ab980afeaf94d01a388d4d1c0124efca814c63f
Abebe <142109195+gitabebe@users.noreply.github.com>
2023-09-14 05:14:02 -0700

    Test Files

---
+++ 0x03-debugging/tests/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * positive_or_negative - prints if integer is positive or negative
+ */
+int void positive_or_negative(int i);
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (n == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
+}
