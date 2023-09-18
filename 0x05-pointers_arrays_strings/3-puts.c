a0283f384185fa6f7729a4c9fb4d7524c0c7ed4d
Abebe <142109195+gitabebe@users.noreply.github.com>
2023-09-18 04:38:34 -0700

    Prints a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_puts - Prints a string, followed by a new line
+ * @str: string to be printed
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
