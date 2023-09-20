0e285c4c32bff61c1ebbf4e92d4a73fb20587897
Abebe <142109195+gitabebe@users.noreply.github.com>
2023-09-20 03:52:28 -0700

    Encodes a string to 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string to 1337.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char l[] = "oOlLeEaAtT";
+	char e[] = "0011334477";
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (l[j] != '\0')
+		{
+			if (s[i] == l[j])
+			{
+				s[i] = e[j];
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (s);
+}
