cda308616bf437e69402ff864aaad0cca3277903
Abebe <142109195+gitabebe@users.noreply.github.com>
2023-09-20 03:52:22 -0700

    Concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates the string pointed to by @src, including the
+ *          terminating null byte, to the end of the string pointed to by @dest
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ *
+ * Return: A pointer to the destination string @dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i++])
+		j++;
+
+	for (i = 0; src[i]; i++)
+		dest[j++] = src[i];
+
+	return (dest);
+}
