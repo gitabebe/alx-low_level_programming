712f85e96d7cf490771d2c6b75b89719766fbfc1
Abebe <142109195+gitabebe@users.noreply.github.com>
2023-10-02 03:08:01 -0700

    prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- ** main - prints the program name, followed by a new line.
- ** @argc: size of program arguments.
- ** @argv: string pointer array to the arguments.
- **
- ** Return: Always o.
+ * main - prints the program name, followed by a new line.
+ * @argc: size of program arguments.
+ * @argv: string pointer array to the arguments.
+ *
+ * Return: Always o.
  **/
 int main(int argc, char *argv[])
 {
