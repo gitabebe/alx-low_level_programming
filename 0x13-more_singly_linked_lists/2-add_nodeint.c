2396eb79158d7341936366637174f3709a234ff9
Abebe <142109195+gitabebe@users.noreply.github.com>
2023-10-23 01:12:31 -0700

    returns the number of elements in a linked listint_t list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_nodeint - adds new node at the beginning of listint_t list
+ * @head: pointer to the address of head of listint_t list
+ * @n: integer to be added to the beginning of listint_t list
+ *
+ * Return: the address of the new element, or Null if the function fails
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
